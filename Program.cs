e2d38196f9d65fd22ca43a4d521164bb4258256d
ALAASALMANA <99395845+ALAASALMANA@users.noreply.github.com>
2022-03-26 16:02:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+
+            Console.WriteLine("Enter the number of hours:");
+                var hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter How much per 1 hour:");
+                var PerHours = Convert.ToDouble(Console.ReadLine());
+           
+            Console.WriteLine("Salary is in a day:" + (hours*PerHours));
+            Console.WriteLine("Salary is in a month:" + (hours * PerHours)*30);
+            Console.WriteLine("Salary is in a 6 month:" + (hours * PerHours) * 30*6);
+            Console.WriteLine("Salary is in a Year:" + (hours * PerHours)* 30*12);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
+
+
